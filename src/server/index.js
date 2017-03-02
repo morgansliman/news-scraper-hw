3a3ff499d60e1a5aa3b083979c8b2afdb27d0b4e
morgansliman <morgansliman@gmail.com>
2017-03-01 21:50:13 -0500

    created article & note models

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,24 @@
 const express    = require('express'),
       bodyParser = require('body-parser'),
-      path       = require('path');
+      path       = require('path'),
+      logger     = require('morgan'),
+      mongoose   = require('mongoose');
 
 const PORT = 8000,
       app  = express();
 
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
 app.use(express.static(path.join(__dirname + '/../client')));
+
+mongoose.Promise = Promise;
+mongoose.set('debug', true);
+mongoose.connect('mongodb://localhost:27017/news');
+
 
 app.listen(PORT, () => {
 	console.log('listening on port:', PORT);
---
+++ src/server/models/Note.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const NoteSchema = new Schema(
+	{
+		title: {type: String},
+		body:  {type: String}
+	}
+);
+
+const Note = mongoose.model('Note', NoteSchema);
+
+module.exports = Note;
